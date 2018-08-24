04273225f489fc2c2833f4dc247650ba6f7768ca
Tsuribori <tsuribori@tutanota.com>
2018-08-24 15:58:42 +0300

    Just a few tests

--- moderation/tests/test_models.py
+++ moderation/tests/test_models.py
@@ -1,14 +1,21 @@
 from django.test import TestCase
 from datetime import timedelta
 from django.utils import timezone
-from moderation.models import Transgression
+from seed.factories import BoardFactory, TransgressionFactory
 # Create your tests here.
 
 class ModelTestCase(TestCase):
   
     def setUp(self):
         future = timezone.now() + timedelta(days=1)
-        self.ban1 = Transgression.objects.create(ip_address='127.0.0.1', banned_until = future, reason='Breaking rules')
+        self.board = BoardFactory()
+        self.ban1 = TransgressionFactory(banned_from=self.board, banned_until=future)
+    
+    def test_no_global_ban(self):
+        self.assertFalse(self.ban1.global_ban)
+ 
+    def test_board_specific_ban(self):
+        self.assertEqual(self.ban1.banned_from, self.board)    
     
     def test_future(self): #Test that ban is in future
         self.assertGreater(self.ban1.banned_until, timezone.now())
